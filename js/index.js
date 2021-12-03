451f8261a26acf86d8c2a3fac435902ea2dd0152
sheikh ammar <sheikhammar568@gmail.com>
2021-12-03 15:22:11 +0500

    First Commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// NAV TOGGLE BUTTON
+var navLinks = document.getElementById("navLinks");
+
+
+
+function showMenu(){
+    navLinks.style.visibility = "visible";
+    navLinks.style.transition = "0.5s";
+}
+function hideMenu(){
+    navLinks.style.visibility ="hidden";
+    navLinks.style.transition = "0.5s";
+}
